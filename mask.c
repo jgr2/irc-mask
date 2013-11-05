762e61b4fac36aeca4755ab5298318b7f313af47
jgr <jgr006@gmail.com>
2013-11-05 11:21:46 +1000

    cleaned up, reformmated

--- mask.c
+++ mask.c
@@ -5,92 +5,32 @@
  */
 /**********************************************************************/
 
-typedef struct {
+typedef struct sbuff sbuff;
+
+struct sbuff {
 	char *p;
 	size_t len, cap;
-} String;
+};
 
-void *string_init (String *s, size_t cap) {
-	
-	if (cap) {
-		s->len = 0;
-		s->cap = cap;
-		s->p = malloc(cap);
-	}
-
-	return s->p;
-}
-
-# define string_free(s) (free((s)->p))
-
-void *string_push (String *s, char c) {
-	char *p;
-
-	if (s->len >= s->cap) {
-		if ((p = realloc(p, s->cap * 2)) == NULL) {
-			return NULL;
-		}
-
-		s->cap *= 2;
-		s->p = p;
-	}
-
-	p = s->p + s->len++;
-	*p = c;
-	*(p+1) = '\0'; // possible write error
-	return p;
-}
-
-# define string_reset(s) ((s)->len = 0, *(s)->p = '\0')
+static void *sbuff_init (sbuff *s, size_t cap);
+static void *sbuff_push(sbuff *s, char c);
+static void *sbuff_xchg(sbuff *s, char c);
+# define sbuff_free(s) (free((s)->p))
+# define sbuff_head(s) (((s)->len) ? *((s)->p + (s)->len - 1): -1)
+static void sbuff_reset(sbuff *s);
 
 /**********************************************************************/
 
-typedef enum Meta Meta;
-enum Meta {EXACT, WILD_ONCE, WILD_MANY, EXIT};
+enum META {EXACT, WILD_ONCE, WILD_MANY, EXIT};
+
+typedef enum META meta;
 
 typedef struct {
-	Meta m;
-	String s;
-} Token;
+	meta m;
+	sbuff s;
+} token;
 
-void gettoken (Token *t, char *p, unsigned *i) {
-
-	unsigned j;
-
-	string_reset(&t->s);
-
-	j = 0;
-	// read a meta
-	if (!p[j]) {
-		t->m = EXIT;
-		return;
-	}
-
-	if (p[j] == '*') {
-		t->m = WILD_MANY;
-	} else if (p[j] == '?') {
-		t->m = WILD_ONCE;
-	} else {
-		string_push(&t->s, p[j]); //
-		t->m = EXACT;
-	}
-	j++;
-
-	// read a sub string
-	while (p[j]) {
-		if (p[j] == '*' || p[j] == '?') {
-			if (t->s.len && t->s.p[t->s.len-1] == '\\') {
-				t->s.p[t->s.len-1] = p[j];
-				j += 1;
-			} else {
-				break;
-			}
-		} else {
-			string_push(&t->s, p[j++]);
-		}
-	}
-	*i += j;
-}
+static long gettoken(token *t, char *p);
 
 int match (char *mask, char *cand) {
 
@@ -98,28 +38,28 @@ int match (char *mask, char *cand) {
 	unsigned i, j;
 
 	char *p;
-	Token t;
+	token t;
 
-	string_init(&t.s, 32);
+	sbuff_init(&t.s, 32);
 	t.m = 0;
 
 	i = 0;
 	j = 0;
 
 	for (;;) {
-		gettoken(&t, mask + i, &i);
+		i += gettoken(&t, mask + i);
 
 		switch (t.m) {
 			case WILD_ONCE:
 				if (cand[j]) {
 					j++;
 				}
-				if (&t.s.len != 0) {
-					goto _EXACT;
+
+				if (&t.s.len == 0) {
+					break;
 				}
-				break;
+				/* fallthrough */
 			case EXACT:
-_EXACT:
 				r = strncmp(t.s.p, cand+j, t.s.len);
 				if (r) {
 					goto RETURN;
@@ -149,9 +89,93 @@ _EXACT:
 		}
 	}
 RETURN:
-	string_free(&t.s);
+	sbuff_free(&t.s);
 	return r;
 }
+
+static long gettoken (token *t, char *p) {
+
+	unsigned j;
+
+	sbuff_reset(&t->s);
+
+	j = 0;
+
+	/* read a meta */
+	if (!p[j]) {
+		t->m = EXIT;
+		return j;
+	}
+
+	if (p[j] == '*') {
+		t->m = WILD_MANY;
+	} else if (p[j] == '?') {
+		t->m = WILD_ONCE;
+	} else {
+		sbuff_push(&t->s, p[j]); 
+		t->m = EXACT;
+	}
+	j++;
+
+	/* read a sub string */
+	while (p[j]) {
+		if (p[j] == '*' || p[j] == '?') {
+			if (t->s.len && sbuff_head(&t->s) == '\\') {
+				sbuff_xchg(&t->s, p[j]);
+				j += 1;
+			} else {
+				break;
+			}
+		} else {
+			sbuff_push(&t->s, p[j++]);
+		}
+	}
+	return j;
+}
+
+static void *sbuff_init (sbuff *s, size_t cap) {
+	
+	if (cap) {
+		s->len = 0;
+		s->cap = cap;
+		s->p = calloc(cap, sizeof *s->p);
+	}
+
+	return s->p;
+}
+
+static void *sbuff_push (sbuff *s, char c) {
+	char *p;
+
+	if (s->len >= s->cap) {
+		if ((p = realloc(s->p, s->cap * 2)) == NULL) {
+			return NULL;
+		}
+
+		s->cap *= 2;
+		s->p = p;
+	}
+
+	p = s->p + s->len++;
+	*p = c;
+	return p;
+}
+
+static void *sbuff_xchg (sbuff *s, char c) {
+
+	if (!s->len) {
+		return NULL;
+	}
+	
+	s->p[s->len-1] = c;
+	return s->p + s->len - 1;
+}
+
+static void sbuff_reset (sbuff *s) {
+	s->len = 0;
+	memset(s->p, '\0', s->cap);
+}
+
 /**********************************************************************/
 
 #include <assert.h>
